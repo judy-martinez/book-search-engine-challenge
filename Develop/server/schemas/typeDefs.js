3e65b1555573ad1bf3bef84dbdb64ef2b491c0b5
Judy <judy.mtz1@gmail.com>
2022-04-19 14:06:11 -0500

    added types & template literal to typeDefs.js

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,32 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type Query {
+    me: User
+}
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+      token: ID!
+      user: User
+  }
+`;
+
+// Template Literal
+module.exports = typeDefs; 
