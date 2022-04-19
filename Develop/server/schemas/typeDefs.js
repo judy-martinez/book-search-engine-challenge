99805e988ad1846b95479fb044f14d23ad47a80b
Judy <judy.mtz1@gmail.com>
2022-04-19 14:15:05 -0500

    added mutation to typedefs

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -5,6 +5,13 @@ type Query {
     me: User
 }
 
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: SavedBook): User
+    removeBook(bookId: String!): User
+}
+
 type User {
     _id: ID
     username: String
